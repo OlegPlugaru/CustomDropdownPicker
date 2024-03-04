e556aa41cc62edac1845c8eb16215a3b48d2a500
olegplugaru <oleg.plugaru.md@gmail.com>
2024-03-04 16:57:35 +0200

    Initial Commit

---
+++ CustomDropdownPicker/CustomDropdownPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDropdownPickerApp.swift
+//  CustomDropdownPicker
+//
+//  Created by Oleg Plugaru on 04.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomDropdownPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
